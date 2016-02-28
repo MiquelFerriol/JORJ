783d0c975549da23dcde7059ce4933d86df53514
MiquelHackUPC <mik_ferri@hotmail.com>
2016-02-29 00:04:28 +0100

    delete

--- JORDJ/src/XAMPP/racestatus.sql
+++ JORDJ/src/XAMPP/racestatus.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-02-2016 a las 20:22:09
+-- Tiempo de generación: 29-02-2016 a las 00:03:59
 -- Versión del servidor: 10.1.10-MariaDB
--- Versión de PHP: 7.0.2
+-- Versión de PHP: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,52 +29,52 @@ SET time_zone = "+00:00";
 CREATE TABLE `racestatus` (
   `id` int(11) NOT NULL,
   `Class` varchar(15) DEFAULT NULL,
-  `Race` int(11) DEFAULT NULL,
+  `Race` int(11) DEFAULT '-1',
   `ScheduledDate` varchar(40) DEFAULT NULL,
   `RealDate` varchar(40) DEFAULT NULL,
-  `Entries` int(11) DEFAULT NULL,
+  `Entries` int(11) DEFAULT '-1',
   `Area` varchar(15) DEFAULT NULL,
   `Committee` varchar(15) DEFAULT NULL,
   `RaceStatus` varchar(20) DEFAULT NULL,
-  `Signal` varchar(10) DEFAULT NULL,
-  `Time` time DEFAULT NULL,
-  `ScheduledTime` time DEFAULT NULL,
-  `StartingTime` time DEFAULT NULL,
-  `BoatsStarted` int(11) DEFAULT NULL,
+  `Signall` varchar(20) DEFAULT NULL,
+  `Time` varchar(15) DEFAULT NULL,
+  `ScheduledTime` varchar(15) DEFAULT NULL,
+  `StartingTime` varchar(15) DEFAULT NULL,
+  `BoatsStarted` int(11) DEFAULT '-1',
   `PreparatorySignal` varchar(10) DEFAULT NULL,
-  `OCS/DSQ` int(11) DEFAULT NULL,
-  `AP` int(11) DEFAULT NULL,
-  `GR` int(11) DEFAULT NULL,
-  `FinishTime` time DEFAULT NULL,
-  `RaceTime` time DEFAULT NULL,
-  `BoatsFinished` int(11) DEFAULT NULL,
+  `OCS_DSQ` int(11) DEFAULT '-1',
+  `AP` int(11) DEFAULT '-1',
+  `GR` int(11) DEFAULT '-1',
+  `FinishTime` varchar(15) DEFAULT NULL,
+  `RaceTime` varchar(15) DEFAULT NULL,
+  `BoatsFinished` int(11) DEFAULT '-1',
   `LastSignal` varchar(15) DEFAULT NULL,
-  `LastSignalTime` time DEFAULT NULL,
+  `LastSignalTime` varchar(15) DEFAULT NULL,
   `Results` varchar(15) DEFAULT NULL,
   `Course` varchar(15) DEFAULT NULL,
-  `Distance1stLeg` int(11) DEFAULT NULL,
-  `Bearing1stLeg` int(11) DEFAULT NULL,
+  `Distance1stLeg` int(11) DEFAULT '-1',
+  `Bearing1stLeg` int(11) DEFAULT '-1',
   `LegChanges` varchar(20) DEFAULT NULL,
-  `WindDir` int(11) DEFAULT NULL,
-  `WindSpeed` int(11) DEFAULT NULL,
-  `WindDir25` int(11) DEFAULT NULL,
-  `WindSpeed25` int(11) DEFAULT NULL,
-  `WindDir50` int(11) DEFAULT NULL,
-  `WindSpeed50` int(11) DEFAULT NULL,
-  `WindDir75` int(11) DEFAULT NULL,
-  `WindSpeed75` int(11) DEFAULT NULL,
-  `WindDir100` int(11) DEFAULT NULL,
-  `WindSpeed100` int(11) DEFAULT NULL
+  `WindDir` int(11) DEFAULT '-1',
+  `WindSpeed` int(11) DEFAULT '-1',
+  `WindDir25` int(11) DEFAULT '-1',
+  `WindSpeed25` int(11) DEFAULT '-1',
+  `WindDir50` int(11) DEFAULT '-1',
+  `WindSpeed50` int(11) DEFAULT '-1',
+  `WindDir75` int(11) DEFAULT '-1',
+  `WindSpeed75` int(11) DEFAULT '-1',
+  `WindDir100` int(11) DEFAULT '-1',
+  `WindSpeed100` int(11) DEFAULT '-1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `racestatus`
 --
 
-INSERT INTO `racestatus` (`id`, `Class`, `Race`, `ScheduledDate`, `RealDate`, `Entries`, `Area`, `Committee`, `RaceStatus`, `Signal`, `Time`, `ScheduledTime`, `StartingTime`, `BoatsStarted`, `PreparatorySignal`, `OCS/DSQ`, `AP`, `GR`, `FinishTime`, `RaceTime`, `BoatsFinished`, `LastSignal`, `LastSignalTime`, `Results`, `Course`, `Distance1stLeg`, `Bearing1stLeg`, `LegChanges`, `WindDir`, `WindSpeed`, `WindDir25`, `WindSpeed25`, `WindDir50`, `WindSpeed50`, `WindDir75`, `WindSpeed75`, `WindDir100`, `WindSpeed100`) VALUES
-(1, 'QWEFQW', 666, '06/12/1995', '06/12/1995', 2, 'Canto', NULL, '', NULL, '-00:00:01', '-00:00:01', '-00:00:01', -1, '', NULL, -1, -1, '-00:00:01', '-00:00:01', -1, '', '-00:00:01', '', '', -1, -1, '', -1, -1, -1, -1, -1, -1, NULL, -1, -1, -1),
-(2, 'QWEFQ', 55, '06/12/1997', '06/12/1995', 2, 'Aero', NULL, '', NULL, '-00:00:01', '-00:00:01', '-00:00:01', -1, '', NULL, -1, -1, '-00:00:01', '-00:00:01', -1, '', '-00:00:01', '', '', -1, -1, '', -1, -1, -1, -1, -1, -1, NULL, -1, -1, -1),
-(3, 'QWEFQ', 5, '', '06/12/1995', -1, 'hola', NULL, '', NULL, '-00:00:01', '-00:00:01', '-00:00:01', -1, '', NULL, -1, -1, '-00:00:01', '-00:00:01', -1, '', '-00:00:01', '', '', -1, -1, '', -1, -1, -1, -1, -1, -1, NULL, -1, -1, -1);
+INSERT INTO `racestatus` (`id`, `Class`, `Race`, `ScheduledDate`, `RealDate`, `Entries`, `Area`, `Committee`, `RaceStatus`, `Signall`, `Time`, `ScheduledTime`, `StartingTime`, `BoatsStarted`, `PreparatorySignal`, `OCS_DSQ`, `AP`, `GR`, `FinishTime`, `RaceTime`, `BoatsFinished`, `LastSignal`, `LastSignalTime`, `Results`, `Course`, `Distance1stLeg`, `Bearing1stLeg`, `LegChanges`, `WindDir`, `WindSpeed`, `WindDir25`, `WindSpeed25`, `WindDir50`, `WindSpeed50`, `WindDir75`, `WindSpeed75`, `WindDir100`, `WindSpeed100`) VALUES
+(1, 'hello', 1, '02/03/2016', '02/02/2016', 5, 'SaoPaulo', 'Pene', 'OK', 'hola', '12:00', '12:00', '12:00:43', 5, '888', 5, 2, 2, '12:18:55', '00:19:55', 2, '2', '12:00', '2', '2', 2, 2, '2', 2, 2, 2, 2, 2, 2, 2, 2, 2, 270),
+(2, 'its me', 5, NULL, NULL, -1, NULL, NULL, NULL, 'asjdfhl', NULL, NULL, NULL, -1, NULL, -1, -1, -1, '', NULL, -1, NULL, NULL, NULL, NULL, -1, -1, NULL, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),
+(3, 'miquel marica', 2, '', NULL, -1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -1, NULL, -1, -1, -1, NULL, NULL, -1, NULL, NULL, NULL, NULL, -1, -1, NULL, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
 
 --
 -- Índices para tablas volcadas
